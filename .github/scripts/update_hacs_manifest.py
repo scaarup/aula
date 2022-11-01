57b888460207f4d6028a30bcbcdf90f76b680a26
Søren Christian Aarup <sc@aarup.org>
2022-11-01 11:45:32 +0100

    Update update_hacs_manifest.py

--- .github/scripts/update_hacs_manifest.py
+++ .github/scripts/update_hacs_manifest.py
@@ -15,48 +15,15 @@ def update_manifest():
             version = str(sys.argv[index + 1]).replace("v", "")
         if value in ["--path", "-P"]:
             manifest_path = str(sys.argv[index + 1])[1:-1]
-        if value in ["--requirements", "-R"]:
-            dorequirements = True
 
     if not manifest_path:
         sys.exit("Missing path to manifest file")
 
-    with open(
-        f"{os.getcwd()}/{manifest_path}/manifest.json",
-        encoding="UTF-8",
-    ) as manifestfile:
+    with open(f"{os.getcwd()}/{manifest_path}/manifest.json",encoding="UTF-8",) as manifestfile:
         manifest = json.load(manifestfile)
-
     manifest["version"] = version
 
-    if dorequirements:
-        requirements = []
-        with open(
-            f"{os.getcwd()}/requirements.txt",
-            encoding="UTF-8",
-        ) as file:
-            for line in file:
-                requirements.append(line.rstrip())
-
-        new_requirements = []
-        for requirement in requirements:
-            req = requirement.split("==")[0].lower()
-            new_requirements = [
-                requirement
-                for x in manifest["requirements"]
-                if x.lower().startswith(req)
-            ]
-            new_requirements += [
-                x for x in manifest["requirements"] if not x.lower().startswith(req)
-            ]
-            manifest["requirements"] = new_requirements
-
-    with open(
-        f"{os.getcwd()}/{manifest_path}/manifest.json",
-        "w",
-        encoding="UTF-8",
-    ) as manifestfile:
+    with open(f"{os.getcwd()}/{manifest_path}/manifest.json","w",encoding="UTF-8",) as manifestfile:
         manifestfile.write(json.dumps(manifest, indent=4, sort_keys=True))
-
 
 update_manifest()
