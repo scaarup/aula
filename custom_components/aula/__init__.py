b0efb5fea20589d362280a20cf07f398916a52c1
Søren Christian Aarup <sc@aarup.org>
2022-11-01 13:31:00 +0100

    Update __init__.py

--- custom_components/aula/__init__.py
+++ custom_components/aula/__init__.py
@@ -3,6 +3,7 @@ Based on https://github.com/JBoye/HA-Aula
 """
 
 from homeassistant.const import CONF_PASSWORD, CONF_USERNAME
+from homeassistant.loader import async_get_integration
 
 from .client import Client
 from .const import DOMAIN, STARTUP
@@ -11,6 +12,7 @@ import logging
 _LOGGER = logging.getLogger(__name__)
 
 async def async_setup(hass, config):
+    integration = await async_get_integration(hass, DOMAIN)
     _LOGGER.info(STARTUP, integration.version)
     conf = config.get(DOMAIN)
     if conf is None:
