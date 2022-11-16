88f0fb859ec411036bb4a7eaa630a59108bedd58
Soren Christian Aarup <scaar@nets.eu>
2022-11-16 13:36:44 +0100

    test

--- custom_components/aula/client.py
+++ custom_components/aula/client.py
@@ -26,7 +26,7 @@ class Client:
     def login(self):
         _LOGGER.debug("Logging in")
         self._session = requests.Session()
-        response = self._session.get('https://login.aula.dk/auth/login.php?type=unilogin', verify=False)
+        response = self._session.get('https://login.aula.dk/auth/login.php?type=unilogin', verify=True)
 
         user_data = { 'username': self._username, 'password': self._password }
         redirects = 0
@@ -43,21 +43,24 @@ class Client:
                         if(input.has_attr('name') and input['name'] == key):
                             post_data[key] = user_data[key]
 
-            response = self._session.post(url, data = post_data, verify=False)
+            response = self._session.post(url, data = post_data, verify=True)
             if response.url == 'https://www.aula.dk:443/portal/':
                 success = True
             redirects += 1
 
-        self._profiles = self._session.get(API + "?method=profiles.getProfilesByLogin", verify=False).json()["data"]["profiles"]
-        self._profilecontext = self._session.get(API + "?method=profiles.getProfileContext&portalrole=guardian", verify=False).json()["data"]
-        
+        self._profiles = self._session.get(API + "?method=profiles.getProfilesByLogin", verify=True).json()["data"]["profiles"]
+        self._profilecontext = self._session.get(API + "?method=profiles.getProfileContext&portalrole=guardian", verify=True).json()["data"]
+        self._relations = self._session.get(API + "?method=profiles.getProfileContext&portalrole=guardian", verify=True).json()["data"]["relations"]
+
+        _LOGGER.debug("type")
+        _LOGGER.debug(type(self._profilecontext))
         # for module in self._profilecontext["moduleWidgetConfiguration"]["moduleConfigurations"]:
         #     if module["module"]["type"] == "presence":
         #         self.presence = 1
         #         break
         #     try:
         #         _LOGGER.debug("Module name: "+str(module["module"]["name"]))
-        #     except:
+        #     except:   
         #         _LOGGER.debug(str(module))
         _LOGGER.debug("LOGIN: " + str(success))
 
@@ -67,7 +70,7 @@ class Client:
         _LOGGER.debug("Presence module: "+str(self.presence))
         is_logged_in = False
         if self._session:
-            response = self._session.get(API + "?method=profiles.getProfilesByLogin", verify=False).json()
+            response = self._session.get(API + "?method=profiles.getProfilesByLogin", verify=True).json()
             is_logged_in = response["status"]["message"] == "OK"
 
         _LOGGER.debug("is_logged_in? " + str(is_logged_in))
@@ -87,13 +90,15 @@ class Client:
         self._daily_overview = {}
    
         for i, child in enumerate(self._children):
-            response = self._session.get(API + "?method=presence.getDailyOverview&childIds[]=" + str(child["id"]), verify=False).json()
+            response = self._session.get(API + "?method=presence.getDailyOverview&childIds[]=" + str(child["id"]), verify=True).json()
             if len(response["data"]) > 0:
+                self.presence = 1337
                 msg = response["data"][0]
                 _LOGGER.debug("msg:"+str(msg))
                 self._daily_overview[str(child["id"])] = response["data"][0]
             else:
                 _LOGGER.debug("data is 0, continuing without getDailyOverview module")
+                self.presence = 0
 
         # Calendar:
         if self._schoolschedule == True:
@@ -107,7 +112,7 @@ class Client:
             post_data = '{"instProfileIds":['+instProfileIds+'],"resourceIds":[],"start":"'+start+'","end":"'+end+'"}'
             _LOGGER.debug("Fetching calendars...")
             _LOGGER.debug("Calendar post-data: "+str(post_data))
-            res = self._session.post(API + "?method=calendar.getEventsByProfileIdsAndResourceIds",data=post_data,headers=headers, verify=False)
+            res = self._session.post(API + "?method=calendar.getEventsByProfileIdsAndResourceIds",data=post_data,headers=headers, verify=True)
             try:
                 with open('skoleskema.json', 'w') as skoleskema_json:
                     json.dump(res.text, skoleskema_json)
@@ -116,16 +121,16 @@ class Client:
         # End of calendar
         # Ugeplaner:
         if self._ugeplan == True:
-            guardian = self._session.get(API + "?method=profiles.getProfileContext&portalrole=guardian", verify=False).json()["data"]["userId"]
+            guardian = self._session.get(API + "?method=profiles.getProfileContext&portalrole=guardian", verify=True).json()["data"]["userId"]
             _LOGGER.debug("guardian :"+str(guardian))
             childUserIds = ",".join(self._childuserids)
-            self._bearertoken = self._session.get(API + "?method=aulaToken.getAulaToken&widgetId=0029", verify=False).json()["data"]
+            self._bearertoken = self._session.get(API + "?method=aulaToken.getAulaToken&widgetId=0029", verify=True).json()["data"]
             token = "Bearer "+str(self._bearertoken)
             self.ugep_attr = {}
             self.ugepnext_attr = {}
             def ugeplan(week,thisnext):
                 get_payload = '/ugebrev?assuranceLevel=2&childFilter='+childUserIds+'&currentWeekNumber='+week+'&isMobileApp=false&placement=narrow&sessionUUID='+guardian+'&userProfile=guardian'
-                ugeplaner = self._session.get(MIN_UDDANNELSE_API + get_payload, headers={"Authorization":token, "accept":"application/json"}, verify=False)
+                ugeplaner = self._session.get(MIN_UDDANNELSE_API + get_payload, headers={"Authorization":token, "accept":"application/json"}, verify=True)
                 _LOGGER.debug("ugeplaner status_code "+str(ugeplaner.status_code))
                 _LOGGER.debug("ugeplaner response "+str(ugeplaner.text))
                 for person in ugeplaner.json()["personer"]:
